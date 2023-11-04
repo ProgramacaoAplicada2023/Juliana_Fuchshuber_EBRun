6f0a248b39d3f671fb7f73141b532758b7a815be
Vinícius Fuchshuber <vinifux@gmail.com>
2023-11-04 17:26:24 -0300

    Adicionando a primeira versão do programa, com a base da interface gráfica

---
+++ EB_Run/EB_Run/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using EB_Run.Core;
+using System;
+
+namespace EB_Run.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+		public HomeViewModel HomeVM{ get; set; }
+		private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set 
+			{ 
+				_currentView = value;
+                OnPropertyChanged();
+			}
+		}
+
+		public MainViewModel()
+		{
+			HomeVM = new HomeViewModel();
+			CurrentView = HomeVM; 
+		}
+
+
+	}
+}
