86cc842874b854927ddc1134e68edd1b7505a935
Vinícius Fuchshuber <vinifux@gmail.com>
2023-11-16 22:37:28 -0300

    TreinoView com botões para escolher o ganho de distância

--- EB_Run/EB_Run/MVVM/View/TreinoView.xaml.cs
+++ EB_Run/EB_Run/MVVM/View/TreinoView.xaml.cs
@@ -24,5 +24,10 @@ namespace EB_Run.MVVM.View
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
