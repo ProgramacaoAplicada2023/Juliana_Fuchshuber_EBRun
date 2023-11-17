01a9a436a9c8f20d9dee45844809cbc54d21c689
Vinícius Fuchshuber <vinifux@gmail.com>
2023-11-17 03:33:19 -0300

    Métodos da classe corrida implementados)

--- EB_Run/EB_Run/Core/Corrida.cs
+++ EB_Run/EB_Run/Core/Corrida.cs
@@ -2,6 +2,7 @@
 using System.Windows.Documents;
 using System.Collections.Generic;
 
+
 namespace EB_Run.Core
 {
     internal class Corrida
@@ -9,6 +10,9 @@ namespace EB_Run.Core
         private int distanciaUltimoTAF = 0;
         private int distanciaAcrescida = 0;
         private int totalSemanasTreino = 0;
+
+        const int distTiro = 400;
+        const int min12 = 12 * 60;
 
         public void inputDistanciaUltimoTAF(int _distanciaUltimoTAF)
         {
@@ -28,55 +32,41 @@ namespace EB_Run.Core
         public List<int> calculaDistanciaRitimada()
         {
             List<int> _distanciasRitimada = new List<int>();
-            int distancia;
+            int distancia = 100*(int)(distanciaUltimoTAF*1.4/100);
 
             for (int i = 0; i< totalSemanasTreino; i++)
             {
-                distancia = distanciaUltimoTAF + distanciaAcrescida * i;
                 _distanciasRitimada.Add(distancia);
+                distancia = 100 * (int)(distancia * 1.1 / 100);
             }
 
             return _distanciasRitimada;
         }
 
-        public List<float> calculaTempoRitimada()
+        public List<int> calculaTempoRitimada()
         {
-            List<float> _temposRitimada = new List<float>();
-            float tempo;
+            List<int> _temposRitimada = new List<int>();
+            int tempo = (int)(min12/((distanciaUltimoTAF+distanciaAcrescida)*0.7));
 
             for (int i = 0; i < totalSemanasTreino; i++)
             {
-                tempo = 20 * i;
                 _temposRitimada.Add(tempo);
+                tempo = (int)(tempo / 1.05);
             }
 
             return _temposRitimada;
         }
-        
-        public List<float> calculaTempoTiro()
+
+
+        public List<int> calculaIntervaloTiro()
         {
-            List<float> _temposTiro = new List<float>();
-            float tempo;
+            List<int> _intervaloTiro = new List<int>();
+            int intervalo = distTiro * min12 / (distanciaUltimoTAF + distanciaAcrescida);
 
-            for(int i = 0;i < totalSemanasTreino; i++)
+            for ( int i = 0; i< totalSemanasTreino; i++)
             {
-                tempo=20 * i;
-                _temposTiro.Add(tempo);
-            }
-
-            return _temposTiro;
-        }
-
-
-        public List<float> calculaIntervaloTiro()
-        {
-            List<float> _intervaloTiro = new List<float>();
-            float intervalo;
-
-            for( int i = 0; i< totalSemanasTreino; i++)
-            {
-                intervalo = 20 * i;
                 _intervaloTiro.Add(intervalo);
+                intervalo = (int)Math.Ceiling(intervalo / 1.1);
             }
 
             return _intervaloTiro;
@@ -85,12 +75,14 @@ namespace EB_Run.Core
         public List<int> calculaRepeticoesTiro()
         {
             List<int> _repeticoesTiro = new List<int>();
-            int repeticoes;
+            double rep = distanciaUltimoTAF/distTiro;
+            int repeticoes = (int)rep;
 
             for(int i=0;  i< totalSemanasTreino; i++)
             {
-                repeticoes = 20 * i;
                 _repeticoesTiro.Add(repeticoes);
+                rep = 1.1 * rep;
+                repeticoes = (int)rep;
             }
 
             return _repeticoesTiro;
